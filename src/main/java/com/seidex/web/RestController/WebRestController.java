98790554ea0279f532e3e96ad64157b294c6ef2f
hwj0623 <hwj0623@naver.com>
2018-09-16 15:05:20 +0900

    프로젝트 생성

---
+++ src/main/java/com/seidex/web/RestController/WebRestController.java
@@ -0,0 +1,13 @@
+package com.seidex.web.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+}
